a2ecbf875af627192d89713d87dc362609c0e014
bczy <bertrand.coizy@gmail.com>
2014-11-30 22:03:12 -0500

    FIxing previous version bug...
    My bad...

--- src/SMath.js
+++ src/SMath.js
@@ -16,40 +16,30 @@ var SMath = function(params) {
 	this.RAD2DEG = 180 / Math.PI;
 	this.DEG2RAD = Math.PI / 180;
 	this.PI2 = Math.PI * 2;
-	this.NB_DEFAULT_VALUES = 3600;
-	
-	var sMath = {};
+	this.NB_DEFAULT_VALUES = 360;
 	
 	// We create cos functions and array only if no params has been passed or nbCos has been specified
 	if (!params || (params && params.nbCos)){
 		this.cosTable = new Float32Array(params && params.nbCos ?  params.nbCos : this.NB_DEFAULT_VALUES);
-		this.fillCache(this.cosTable);
+		this.fillCache(this.cosTable, Math.cos);
 		SMath.prototype.cos = function (angle) {
-			angle %= this.PI2;
-			if (angle < 0) 
-				angle += this.PI2;
-			angle = angle * this.RAD2DEG;
-			return this.cosTable[angle | 0];
+			return this.cosTable[(angle * this.cosTable.length / this.PI2) | 0];
 		};
 	}
 
 	if (params && params.nbSin){
 		this.sinTable = new Float32Array(params.nbSin);
-		this.fillCache(this.sinTable);
+		this.fillCache(this.sinTable, Math.sin);
 		SMath.prototype.sin = function (angle) {
-			angle %= this.PI2;
-			if (angle < 0) 
-				angle += this.PI2;
-			angle = angle * this.RAD2DEG;
-			return this.sinTable[angle | 0];
+			return this.sinTable[(angle * this.sinTable.length / this.PI2) | 0];
 		};
 	}
 };
 
-SMath.prototype.fillCache = function(array){
-	console.log(array.length)
-	for (var i = array.length -1; i >= 0 ; i--) {
-		this.cosTable[i] = Math.cos(i * this.DEG2RAD);
+SMath.prototype.fillCache = function(array,mathFunction){
+	var length = array.length;
+	for (var i = 0 ; i < this.PI2 ; i += this.PI2 / length) {
+		array[(i * length / this.PI2) | 0] = mathFunction(i);
 	}
 }
-
+	
