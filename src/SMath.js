a4e4b442b475306b9b3c67c88af7eef68d69280b
Dan Kaplun <dan.kaplun@essencedigital.com>
2015-11-19 09:32:29 -0500

    Fixes lack of SMath prototype methods before instantiating an instance
    Also general refactoring

--- src/SMath.js
+++ src/SMath.js
@@ -22,45 +22,38 @@
 		Note: if either sin or cos has been calculated, a default cosine array will be created
 		TODO: atan things
 	*/
-	var SMath = function(params) {
-		// Constants
-		this.RAD2DEG = 180 / Math.PI;
-		this.DEG2RAD = Math.PI / 180;
-		this.PI2 = Math.PI * 2;
-	
+	var PI2 = Math.PI * 2;
+
+	function SMath (params) {
+		this.params = params = params || {};
 		this.nbCos = params.nbCos || 360;
 		this.nbSin = params.nbSin || 360;
-		// We create cos functions and array only if no params has been passed or nbCos has been specified
-		if (!params || (params && params.nbCos)) {
-			this.cosTable = new Float32Array(this.nbCos);
-			// The cos factor is the difference in radians between two lookup values
-			this.cosFactor = this.nbCos / this.PI2;
-			this.fillCache(this.cosTable, this.cosFactor, Math.cos);
-			SMath.prototype.cos = function (angle) {
-				angle %= this.PI2;
-				if (angle < 0) angle += this.PI2;
-				return this.cosTable[(angle * this.cosFactor) | 0];
-			};
-		}
-	
-		if (!params || (params && params.nbSin)) {
-			this.sinTable = new Float32Array(this.nbSin);
-			this.sinFactor = this.nbSin / this.PI2;
-			this.fillCache(this.sinTable, this.sinFactor, Math.sin);
-			SMath.prototype.sin = function (angle) {
-				angle %= this.PI2;
-				if (angle < 0) angle += this.PI2;
-				return this.sinTable[(angle * this.sinFactor) | 0];
-			};
-		}
+
+		this.cosTable = new Float32Array(this.nbCos);
+		this.cosFactor = this.nbCos / PI2;
+		SMath.fillCache(this.cosTable, this.cosFactor, Math.cos);
+
+		this.sinTable = new Float32Array(this.nbSin);
+		this.sinFactor = this.nbSin / PI2;
+		SMath.fillCache(this.sinTable, this.sinFactor, Math.sin);
 	};
-	
-	SMath.prototype.fillCache = function(array, factor, mathFunction){
+	SMath.prototype.cos = function (angle) {
+		angle %= PI2;
+		if (angle < 0) angle += PI2;
+		return this.cosTable[(angle * this.cosFactor) | 0];
+	};
+	SMath.prototype.sin = function (angle) {
+		angle %= PI2;
+		if (angle < 0) angle += PI2;
+		return this.sinTable[(angle * this.sinFactor) | 0];
+	};
+
+	SMath.fillCache = function (array, factor, mathFunction) {
 		var length = array.length;
-		for (var i = 0 ; i < length ; i += 1) {
+		for (var i = 0; i < length; i++) {
 			array[i] = mathFunction(i / factor);
 		}
-	}
+	};
 
 	return SMath;
 }));
