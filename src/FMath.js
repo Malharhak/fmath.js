b7a4753e792856de9498d46d136a94909df5daaa
Anthony Pigeot <anthony.pigeot@gmail.com>
2015-11-20 21:23:06 +0100

    Renamed SMath to FMath (automatic replace, preserving the case)

--- src/FMath.js
+++ src/FMath.js
@@ -1,60 +1,60 @@
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		define(function () {
-			return (root.SMath = factory());
+			return (root.FMath = factory());
 		});
 	} else if (typeof module === 'object' && module.exports) {
 		module.exports = factory();
 	} else {
-		root.SMath = factory();
+		root.FMath = factory();
 	}
 }(this, function () {
 	var PI2 = Math.PI * 2;
 
-	SMath.DEFAULT_PARAMS = {
+	FMath.DEFAULT_PARAMS = {
 		resolution: 360,
 		minAtan: -40,
 		maxAtan: 40
 	};
 
 	/**
-	 * SMath constructor
+	 * FMath constructor
 	 * @param {Object} params - passed to the constructor
 	 * @param {number} [params.resolution] - # of cached values for any function. Is overriden by optional specific values
-	 * @param {number} [params.nbSin] - # of cached values for SMath#sin (defaults to the resolution)
-	 * @param {number} [params.nbCos] - # of cached values for SMath#cos (defaults to the resolution)
-	 * @param {number} [params.nbAtan] - # of caches values for SMath#atan (defaults to the resolution)
+	 * @param {number} [params.nbSin] - # of cached values for FMath#sin (defaults to the resolution)
+	 * @param {number} [params.nbCos] - # of cached values for FMath#cos (defaults to the resolution)
+	 * @param {number} [params.nbAtan] - # of caches values for FMath#atan (defaults to the resolution)
 	 * @param {number} [params.minAtan] - Minimal value for the caching of atan (default: -20) - If asking a lower value, will return the lowest known
 	 * @param {number} [params.maxAtan] - Maximal value for the caching of atan (default: 20) - If asking ahigher value, will return the highest known
 	 */
-	function SMath (params) {
-		this.params = SMath._assign(null, SMath.DEFAULT_PARAMS, params);
-		SMath._setDefaultValues(this.params);
+	function FMath (params) {
+		this.params = FMath._assign(null, FMath.DEFAULT_PARAMS, params);
+		FMath._setDefaultValues(this.params);
 
 		this.cosTable = new Float32Array(this.params.nbCos);
 		this.cosFactor = this.params.nbCos / PI2;
-		SMath._fillCache(this.cosTable, this.cosFactor, Math.cos);
+		FMath._fillCache(this.cosTable, this.cosFactor, Math.cos);
 
 		this.sinTable = new Float32Array(this.params.nbSin);
 		this.sinFactor = this.params.nbSin / PI2;
-		SMath._fillCache(this.sinTable, this.sinFactor, Math.sin);
+		FMath._fillCache(this.sinTable, this.sinFactor, Math.sin);
 
 		this.atanTable = new Float32Array(this.params.nbAtan);
 		this.atanFactor = this.params.nbAtan / (this.params.maxAtan - this.params.minAtan)
-		SMath._fillAtanCache(this.atanTable, this.atanFactor, this.params.minAtan);
+		FMath._fillAtanCache(this.atanTable, this.atanFactor, this.params.minAtan);
 	};
 
-	SMath.prototype.cos = function (angle) {
+	FMath.prototype.cos = function (angle) {
 		angle %= PI2;
 		if (angle < 0) angle += PI2;
 		return this.cosTable[(angle * this.cosFactor) | 0];
 	};
-	SMath.prototype.sin = function (angle) {
+	FMath.prototype.sin = function (angle) {
 		angle %= PI2;
 		if (angle < 0) angle += PI2;
 		return this.sinTable[(angle * this.sinFactor) | 0];
 	};
-	SMath.prototype.atan = function (tan) {
+	FMath.prototype.atan = function (tan) {
 		var index = ((tan - this.params.minAtan) * this.atanFactor) | 0;
 		if (index < 0) {
 			return - Math.PI / 2;
@@ -64,7 +64,7 @@
 		return this.atanTable[index];
 	};
 
-	SMath._setDefaultValues = function (params) {
+	FMath._setDefaultValues = function (params) {
 		var functionNames = ["nbSin", "nbCos", "nbAtan"];
 		for (var i = functionNames.length - 1; i >= 0; i--) {
 			var key = functionNames[i];
@@ -72,21 +72,21 @@
 		}
 	};
 
-	SMath._fillAtanCache = function (array, factor, min) {
+	FMath._fillAtanCache = function (array, factor, min) {
 		for (var i = 0; i < array.length; i++) {
 			var tan = min + i / factor;
 			array[i] = Math.atan(tan);
 		}
 	};
 
-	SMath._fillCache = function (array, factor, mathFunction) {
+	FMath._fillCache = function (array, factor, mathFunction) {
 		var length = array.length;
 		for (var i = 0; i < length; i++) {
 			array[i] = mathFunction(i / factor);
 		}
 	};
 
-	SMath._assign = function (dst, src1, src2, etc) {
+	FMath._assign = function (dst, src1, src2, etc) {
 		return [].reduce.call(arguments, function (dst, src) {
 			src = src || {};
 			for (var k in src) {
@@ -98,5 +98,5 @@
 		}, dst || {});
 	};
 
-	return SMath;
+	return FMath;
 }));
